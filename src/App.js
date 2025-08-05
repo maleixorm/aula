25da1e4615224f1489adce68c385948a5577bc3f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-04 23:36:56 -0300

    Criando uma requisição http para consumir dados de uma api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './style.css';
+
+// https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+      fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        setNutri(json);
+      })
+    }
+    loadApi();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
       </header>
+      {nutri.map((item) => {
+        return(
+          <article key={item.id} className="posts">
+            <strong className="titulo">{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className="capa" />
+            <p className="subtitulo">{item.subtitulo}</p>
+            <p className="categoria"><strong>Categoria: {item.categoria}</strong></p>
+            <a className="btn" href="#">Acessar</a>
+          </article>
+        )
+      })}
     </div>
   );
 }
